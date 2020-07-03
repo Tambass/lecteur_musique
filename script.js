d15511fd9a08d41f65b13f58aebc32dbc99fbddd
Tambass <tambass85@gmail.com>
2020-07-03 14:11:52 +0200

    arrangement playlist

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const musicContainer = document.getElementById("music-container");
-const playBtn = document.getElementById("play");
-const prevBtn = document.getElementById("prev");
-const nextBtn = document.getElementById("next");
-const audio = document.getElementById("audio");
-const progress = document.getElementById("progress");
-const progressContainer = document.getElementById("progress-container");
-const title = document.getElementById("title");
-const cover = document.getElementById("cover");
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
 
-// Songs titles
-const songs = ["mr_bungle", "ratm", "ultra_vomit"];
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+// Song titles
+const songs = ['mrbungle', 'ratm', 'ultravomit'];
 
 // Keep track of song
 let songIndex = 2;
@@ -20,24 +21,24 @@ loadSong(songs[songIndex]);
 // Update song details
 function loadSong(song) {
   title.innerText = song;
-  audio.scr = `musics/${song}.mp3`;
-  cover.scr = `images/${song}.jpg`;
+  audio.src = `musics/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
 }
 
 // Play song
 function playSong() {
-  musicContainer.classList.add("play");
-  playBtn.querySelector("i.fas").classList.remove("fa-play");
-  playBtn.querySelector("i.fas").classList.add("fa-pause");
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
 
   audio.play();
 }
 
 // Pause song
 function pauseSong() {
-  musicContainer.classList.remove("play");
-  playBtn.querySelector("i.fas").classList.add("fa-play");
-  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
 
   audio.pause();
 }
@@ -54,6 +55,7 @@ function prevSong() {
 
   playSong();
 }
+
 // Next song
 function nextSong() {
   songIndex++;
@@ -84,8 +86,8 @@ function setProgress(e) {
 }
 
 // Event listeners
-playBtn.addEventListener("click", () => {
-  const isPlaying = musicContainer.classList.contains("play");
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
 
   if (isPlaying) {
     pauseSong();
@@ -95,14 +97,14 @@ playBtn.addEventListener("click", () => {
 });
 
 // Change song
-prevBtn.addEventListener("click", prevSong);
-nextBtn.addEventListener("click", nextSong);
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
 
-// Time/song update event
-audio.addEventListener("timeupdate", updateProgress);
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
 
 // Click on progress bar
-progressContainer.addEventListener("click", setProgress);
+progressContainer.addEventListener('click', setProgress);
 
-// Song end
-audio.addEventListener('ended', nextSong)
+// Song ends
+audio.addEventListener('ended', nextSong);
